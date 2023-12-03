94e330be4bae60f996cb18f38e2e69c633e1fc75
karma <ktenzleo@gmail.com>
2023-12-03 16:36:18 +0600

    chnage release

--- release.ps1
+++ release.ps1
@@ -44,7 +44,7 @@ if (Test-Path $outDir) {
 Push-Location $projDir
 try {
     Write-Output "Restoring:"
-    dotnet restore -r win-x64
+    dotnet build -r win-x64 -c Release
     Write-Output "Publishing:"
     $msBuildVerbosityArg = "/v:m"
     if ($env:CI) {
