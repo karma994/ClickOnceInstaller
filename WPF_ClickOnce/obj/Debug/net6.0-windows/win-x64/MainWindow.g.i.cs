1523d0ac8d915424fb9be4c321232a4b9c36b9dd
karma <ktenzleo@gmail.com>
2023-12-04 11:48:25 +0600

    chnage

--- WPF_ClickOnce/obj/Debug/net6.0-windows/win-x64/MainWindow.g.i.cs
+++ WPF_ClickOnce/obj/Debug/net6.0-windows/win-x64/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace WPF_ClickOnce {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPF_ClickOnce;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPF_ClickOnce;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
